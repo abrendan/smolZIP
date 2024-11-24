547eedb5d486ca40b38ad70a299d52b4696a3581
abrendan <brendangass.2@gmail.com>
2024-11-24 02:57:56 +0100

    Add project files.

---
+++ smolZIP/MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace smolZIP
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnExtractZip;
+        private System.Windows.Forms.Button btnCreateZip;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblVersion;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnExtractZip = new System.Windows.Forms.Button();
+            this.btnCreateZip = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExtractZip
+            // 
+            this.btnExtractZip.BackColor = System.Drawing.Color.Transparent;
+            this.btnExtractZip.Location = new System.Drawing.Point(12, 325);
+            this.btnExtractZip.Name = "btnExtractZip";
+            this.btnExtractZip.Size = new System.Drawing.Size(109, 43);
+            this.btnExtractZip.TabIndex = 2;
+            this.btnExtractZip.Text = "Extract ZIP";
+            this.btnExtractZip.UseVisualStyleBackColor = false;
+            this.btnExtractZip.Click += new System.EventHandler(this.btnExtractZip_Click);
+            // 
+            // btnCreateZip
+            // 
+            this.btnCreateZip.Location = new System.Drawing.Point(190, 325);
+            this.btnCreateZip.Name = "btnCreateZip";
+            this.btnCreateZip.Size = new System.Drawing.Size(104, 43);
+            this.btnCreateZip.TabIndex = 3;
+            this.btnCreateZip.Text = "Create ZIP";
+            this.btnCreateZip.UseVisualStyleBackColor = true;
+            this.btnCreateZip.Click += new System.EventHandler(this.btnCreateZip_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 285);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(282, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Font = new System.Drawing.Font("Space Mono", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(21, 109);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(259, 28);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "smolZIP by abrendan";
+            this.lblTitle.Click += new System.EventHandler(this.lblTitle_Click);
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.BackColor = System.Drawing.Color.Transparent;
+            this.lblVersion.ForeColor = System.Drawing.Color.White;
+            this.lblVersion.Location = new System.Drawing.Point(225, 269);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(69, 13);
+            this.lblVersion.TabIndex = 1;
+            this.lblVersion.Text = "Version 1.0.0";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = global::smolZIP.Properties.Resources.background1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(306, 380);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnCreateZip);
+            this.Controls.Add(this.btnExtractZip);
+            this.Name = "MainForm";
+            this.Text = "smolZIP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ smolZIP/MainForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace smolZIP
+{
+    public partial class MainForm : Form
+    {
+        private const string AppVersion = "1.0.0";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // Extract ZIP File with progress
+        private void ExtractZip(string zipFilePath, string extractPath)
+        {
+            try
+            {
+                using (FileStream fs = File.OpenRead(zipFilePath))
+                {
+                    ZipFile zf = new ZipFile(fs);
+                    progressBar.Maximum = (int)zf.Count;
+                    progressBar.Value = 0;
+
+                    foreach (ZipEntry entry in zf)
+                    {
+                        if (!entry.IsFile) continue;
+
+                        string entryFileName = entry.Name;
+                        byte[] buffer = new byte[4096];
+
+                        // ZipFile.GetInputStream() returns a raw input stream.
+                        using (Stream zipStream = zf.GetInputStream(entry))
+                        {
+                            string fullZipToPath = Path.Combine(extractPath, entryFileName);
+                            string directoryName = Path.GetDirectoryName(fullZipToPath);
+
+                            if (directoryName.Length > 0)
+                                Directory.CreateDirectory(directoryName);
+
+                            using (FileStream streamWriter = File.Create(fullZipToPath))
+                            {
+                                StreamUtils.Copy(zipStream, streamWriter, buffer);
+                            }
+                        }
+
+                        progressBar.Value++;
+                    }
+                }
+                MessageBox.Show("Extraction complete!", "smolZIP", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                progressBar.Value = 0;
+            }
+        }
+
+        // Create ZIP File with progress
+        private void CreateZip(string folderPath, string zipFilePath)
+        {
+            try
+            {
+                FastZipEvents events = new FastZipEvents();
+                events.Progress = new ProgressHandler((source, progress) =>
+                {
+                    progressBar.Value = progressBar.Value + 1;
+                });
+
+                FastZip fastZip = new FastZip(events);
+                fastZip.CreateZip(zipFilePath, folderPath, true, null);
+                MessageBox.Show("Archiving complete!", "smolZIP", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                progressBar.Value = 0;
+            }
+        }
+
+        private void btnExtractZip_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Zip files (*.zip)|*.zip";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+                    {
+                        if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            ExtractZip(openFileDialog.FileName, folderBrowserDialog.SelectedPath);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnCreateZip_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                    {
+                        saveFileDialog.Filter = "Zip files (*.zip)|*.zip";
+                        if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            CreateZip(folderBrowserDialog.SelectedPath, saveFileDialog.FileName);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void lblTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
